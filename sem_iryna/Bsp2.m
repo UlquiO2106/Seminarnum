b8f39b77cde3e23ca924c02bc87921f4de34ff14
unknown <UlquiO2106>
2018-01-08 14:15:57 +0100

    geloscht

--- sem_iryna/Bsp2.m
+++ sem_iryna/Bsp2.m
@@ -1,37 +1,26 @@
-% n1=9;
-% Ahilf1=4*eye(n1,n1)+(-1)*diag(ones(n1-1,1),1) + (-1)*diag(ones(n1-1,1),-1) +(-1)*diag(ones(n1-3,1),3) +(-1)*diag(ones(n1-3,1),-3);
-% hilfstemp1= zeros(n1-1,1);
-% for i=1:n1-1
-%     if mod(i,3)==0
-%         hilfstemp1(i)=1;
-%     end
-% end
-% temp=diag(hilfstemp1,1) + diag(hilfstemp1,-1);
-% A1=Ahilf1+temp;
-% 
-% n2=12;
-% Ahilf2=4*eye(n2,n2)+(-1)*diag(ones(n2-1,1),1) + (-1)*diag(ones(n2-1,1),-1) +(-1)*diag(ones(n2-3,1),3) +(-1)*diag(ones(n2-3,1),-3);
-% hilfstemp2= zeros(n2-1,1);
-% for i=1:n2-1
-%     if mod(i,3)==0
-%         hilfstemp2(i)=1;
-%     end
-% end
-% temp=diag(hilfstemp2,1) + diag(hilfstemp2,-1);
-% A2=Ahilf2+temp;
-% 
-% n3=12;
-% Ahilf2=4*eye(n3,n3)+(-1)*diag(ones(n3-1,1),1) + (-1)*diag(ones(n3-1,1),-1) +(-1)*diag(ones(n3-3,1),3) +(-1)*diag(ones(n3-3,1),-3);
-% hilfstemp3= zeros(n3-1,1);
-% for i=1:n3-1
-%     if mod(i,3)==0
-%         hilfstemp3(i)=1;
-%     end
-% end
-% temp=diag(hilfstemp3,1) + diag(hilfstemp3,-1);
-% A3=Ahilf3+temp;
-
-%%
-A1=innereMatrix(9,3)
-A2=innereMatrix(12,3);
-A3=innereMatrix(12,4);
+%Anzahl innere Punkte des gebietes
+n1=9;
+n2=12;
+n3=12;
+%innere Matrix der Gebiete
+A1=innereMatrix(n1,3,1);
+A2=innereMatrix(n2,3,1);
+A3=innereMatrix(n3,4,2);
+%die Ei
+E1=[0 0 0 0 0 0 0; 0 0 0 0 0 0 0; -1 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 -1 0 0 0 0 0; 0 0 0 0 0 0 -1; 0 0 0 0 0 -1 0; 0 0 -1 0 -1 0 0;];
+E2=[-1 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 -1 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 -1 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 -1 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0;];
+E3=[0 0 0 0 0 0 -1; 0 0 0 0 0 -1 0; 0 0 0 0 -1 0 0; 0 0 0 -1 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0; 0 0 0 0 0 0 0;];
+%Zeilen Matrizen
+AA1=[A1,zeros(n1,n2),zeros(n1,n3),E1];
+AA2=[zeros(n2,n1),A2,zeros(n2,n3),E2];
+AA3=[zeros(n3,n1),zeros(n3,n2),A3,E3];
+%die Fj
+F1=[0 0 -1 0 0 0 0 0 0; 0 0 0 0 0 -1 0 0 0; 0 0 0 0 0 0 0 0 -1; 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 -1; 0 0 0 0 0 0 0 -1 0; 0 0 0 0 0 0 -1 0 0;];
+F2=[-1 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 -1 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 -1 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 -1 0 0; 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0;];
+F3=[0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 -1 0 0 0 0 0 0 0 0; 0 0 -1 0 0 0 0 0 0 0 0 0; 0 -1 0 0 0 0 0 0 0 0 0 0; -1 0 0 0 0 0 0 0 0 0 0 0;];
+%die C
+C=innereMatrix(7,10,-1);
+%die letzte Zeile
+FC=[F1,F2,F3,C];
+%die A
+A=transpose([transpose(AA1),transpose(AA2),transpose(AA3),transpose(FC)]);
--- sem_iryna/innereMatrix.m
+++ sem_iryna/innereMatrix.m
@@ -1,13 +1,24 @@
-function [A1] = innereMatrix(n1,j)
-Ahilf1=4*eye(n1,n1)+(-1)*diag(ones(n1-1,1),1) + (-1)*diag(ones(n1-1,1),-1) +(-1)*diag(ones(n1-3,1),3) +(-1)*diag(ones(n1-3,1),-3);
-hilfstemp1= zeros(n1-1,1);
-for i=1:n1-1
-    if mod(i,j)==0
-        hilfstemp1(i)=1;
+function [A1] = innereMatrix(n1,j,k)
+if k == -1
+    Ahilf1=4*eye(n1,n1)+(-1)*diag(ones(n1-1,1),1) + (-1)*diag(ones(n1-1,1),-1);
+    hilfstemp1= zeros(n1-1,1);
+    for i=1:n1-1
+        if mod(i,j)==0
+            hilfstemp1(i)=1;
+        end
     end
+    temp=diag(hilfstemp1,1) + diag(hilfstemp1,-1);
+    A1=Ahilf1+temp;  
+else
+    Ahilf1=4*eye(n1,n1)+(-1)*diag(ones(n1-1,1),1) + (-1)*diag(ones(n1-1,1),-1) +(-1)*diag(ones(n1-(k+2),1),k+2) +(-1)*diag(ones(n1-(k+2),1),-k-2);
+    hilfstemp1= zeros(n1-1,1);
+    for i=1:n1-1
+        if mod(i,j)==0
+            hilfstemp1(i)=1;
+        end
+    end
+    temp=diag(hilfstemp1,1) + diag(hilfstemp1,-1);
+    A1=Ahilf1+temp;
 end
-temp=diag(hilfstemp1,1) + diag(hilfstemp1,-1);
-A1=Ahilf1+temp;
-
 end
 
